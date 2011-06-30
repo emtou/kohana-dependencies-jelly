659ff319fa31b4933cc7c6f4072831b8b6eadc83
mtou <mtou@charougna.com>
2011-06-30 14:41:22 +0200

    Add empty Jelly

---
+++ classes/jelly.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Declares Jelly
+ *
+ * PHP version 5
+ *
+ * @group DependenciesJelly
+ *
+ * @category  DependenciesJelly
+ * @package   DependenciesJelly
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-dependencies-jelly/tree/master/classes/jelly.php
+ * @since     2011-06-30
+ */
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * Provides Jelly
+ *
+ * PHP version 5
+ *
+ * @group DependenciesJelly
+ *
+ * @category  DependenciesJelly
+ * @package   DependenciesJelly
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-chacms/tree/master/classes/jelly.php
+ */
+abstract class Jelly extends Jelly_Core
+{
+
+}
