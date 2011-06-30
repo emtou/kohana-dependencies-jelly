a90f8695138e401a5321b0f95674eee90bd030cf
mtou <mtou@charougna.com>
2011-06-30 15:01:13 +0200

    Added empty Jelly_Collection

---
+++ classes/jelly/collection.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Declares Jelly_Collection
+ *
+ * PHP version 5
+ *
+ * @group DependenciesJelly
+ *
+ * @category  DependenciesJelly
+ * @package   DependenciesJelly
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-dependencies-jelly/tree/master/classes/jelly/collection.php
+ * @since     2011-06-30
+ */
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * Provides Jelly_Collection
+ *
+ * PHP version 5
+ *
+ * @group DependenciesJelly
+ *
+ * @category  DependenciesJelly
+ * @package   DependenciesJelly
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-chacms/tree/master/classes/jelly/collection.php
+ */
+class Jelly_Collection extends Jelly_Core_Collection
+{
+
+}
