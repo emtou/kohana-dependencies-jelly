235f76fd9853c2da4b088ce818d55adc1f4922b5
mtou <mtou@charougna.com>
2011-07-01 14:41:13 +0200

    Enable dependency injection in Jelly_Collection constructor

--- classes/jelly/collection.php
+++ classes/jelly/collection.php
@@ -18,7 +18,7 @@
 defined('SYSPATH') OR die('No direct access allowed.');
 
 /**
- * Provides Jelly_Collection
+ * Overloads Jelly_Collection to enable dependency injection
  *
  * PHP version 5
  *
@@ -33,5 +33,39 @@ defined('SYSPATH') OR die('No direct access allowed.');
  */
 class Jelly_Collection extends Jelly_Core_Collection
 {
+  /**
+   * Tracks a database result
+   *
+   * @param mixed                $result         database result
+   * @param mixed                $model          model to retun results as
+   * @param Dependency_Container $container      optional DI
+   * @param string               $definition_key optional definition key for the DI
+   */
+  public function __construct($result, $model = NULL, $container = NULL, $definition_key = NULL)
+  {
+    $this->_result = $result;
 
+    // Load our default model
+    if ($model AND Jelly::meta($model))
+    {
+      if ($model instanceof Jelly_Model)
+      {
+        $this->_model = $model;
+      }
+      else
+      {
+        if ($definition_key)
+        {
+          $this->_model            = $container->get($definition_key);
+          $this->_model->container = $container;
+        }
+        else
+        {
+          $this->_model = new $model;
+        }
+      }
+
+      $this->_meta = $this->_model->meta();
+    }
+  }
 }
