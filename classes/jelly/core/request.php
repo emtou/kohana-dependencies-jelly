7f4956c56954cb2ea9f03bacf7b02b668429d814
mtou <mtou@charougna.com>
2011-07-01 12:19:22 +0200

    Add Jelly_Request declaration

---
+++ classes/jelly/core/request.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Declares Jelly_Core_Request
+ *
+ * PHP version 5
+ *
+ * @group DependenciesJelly
+ *
+ * @category  DependenciesJelly
+ * @package   DependenciesJelly
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-dependencies-jelly/tree/master/classes/jelly/core/request.php
+ * @since     2011-07-01
+ */
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * Provides Jelly_Core_Request
+ *
+ * PHP version 5
+ *
+ * @group DependenciesJelly
+ *
+ * @category  DependenciesJelly
+ * @package   DependenciesJelly
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-dependencies-jelly/tree/master/classes/jelly/core/request.php
+ */
+abstract class Jelly_Core_Request
+{
+
+  public function __construct()
+  {
+
+  }
+
+} // End class Jelly_Core_Request
---
+++ classes/jelly/request.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Declares Jelly_Request
+ *
+ * PHP version 5
+ *
+ * @group DependenciesJelly
+ *
+ * @category  DependenciesJelly
+ * @package   DependenciesJelly
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-dependencies-jelly/tree/master/classes/jelly/request.php
+ * @since     2011-07-01
+ */
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * Provides Jelly_Request
+ *
+ * PHP version 5
+ *
+ * @group DependenciesJelly
+ *
+ * @category  DependenciesJelly
+ * @package   DependenciesJelly
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-dependencies-jelly/tree/master/classes/jelly/request.php
+ */
+class Jelly_Request extends Jelly_Core_Request
+{
+
+} // End class Jelly_Request
